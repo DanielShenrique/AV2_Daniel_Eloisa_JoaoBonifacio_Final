110df2a315df510f9ad50182ecb9b44b041f5f17
DanielShenrique <Daniel.Henrique@cejoseleitelopes.com.br>
2017-08-30 11:05:14 -0300

    AV2_Final

---
+++ AV2_Daniel_Eloisa_JoãoBonifácio/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AV2_Daniel_Eloisa_JoãoBonifácio
+{
+    public partial class Form1 : Form
+    {
+        public int Dano;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ataque = (int) numericUpDown1.Value;
+
+            if(comboBox1.Text == "Eletricidade" && comboBox2.Text == "Fogo")
+            {
+                 ataque = 2 * ataque;
+            }
+
+            if(comboBox1.Text == "Fogo" && comboBox2.Text == "Gelo")
+            {
+                ataque = 2 * ataque;
+            }
+
+            if(comboBox1.Text == "Gelo" && comboBox2.Text == "Eletricidade")
+            {
+                ataque = 2 * ataque;
+            }
+
+            int defesa = (int) numericUpDown2.Value;
+
+            if (comboBox1.Text == "Fogo" && comboBox2.Text == "Eletricidade")
+            {
+                defesa = 2 * defesa;
+            }
+            if (comboBox1.Text == "Gelo" && comboBox2.Text == "Fogo")
+            {
+                defesa = 2 * defesa;
+            }
+            if (comboBox1.Text == "Eletricidade" && comboBox2.Text == "Gelo")
+            {
+                defesa = 2 * defesa;
+            }
+
+
+
+            Dano = ataque - defesa;
+
+            if(ataque < defesa)
+            {
+                Dano = 0;
+            }
+
+            label1.Text = Dano.ToString();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+                   
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AV2_Daniel_Eloisa_JoãoBonifácio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AV2_Daniel_Eloisa_JoãoBonifácio
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
